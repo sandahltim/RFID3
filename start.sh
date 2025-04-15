35d47884ff676fbc4d5ec03c9d92f946b5366c1a
sandahltim <tim@broadwaytent.com>
2025-04-14 22:25:48 -0500

    2

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+# Check if it uses sudo; if so, modify to run as tim
+# Example start.sh:
 #!/bin/bash
-source venv/bin/activate
-gunicorn --workers 2 --bind 0.0.0.0:7409 run:app
+source /home/tim/test_rfidpi/venv/bin/activate
+gunicorn --workers 2 --bind 0.0.0.0:7409 run:app --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log --log-level debug
