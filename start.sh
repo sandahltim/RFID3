ec73686b3852449e3002c03388cf658ea89909c0
SMOK33Y3 <73911129+SMOK33Y3@users.noreply.github.com>
2025-04-15 11:45:08 -0500

    Reduce Gunicorn workers to 1 to prevent database lock

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/tim/test_rfidpi/venv/bin/activate
-exec gunicorn --workers 2 --bind 0.0.0.0:7409 --timeout 60 run:app --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log --log-level debug
+exec gunicorn --workers 1 --bind 0.0.0.0:7409 --timeout 60 run:app --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log --log-level debug
