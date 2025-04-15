3f837923f6f1dc103815828ba84dc02b0b0529cd
SMOK33Y3 <73911129+SMOK33Y3@users.noreply.github.com>
2025-04-15 10:51:51 -0500

    Fix start.sh execution for rfid_dash service

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-# Check if it uses sudo; if so, modify to run as tim
-# Example start.sh:
 #!/bin/bash
 source /home/tim/test_rfidpi/venv/bin/activate
-gunicorn --workers 2 --bind 0.0.0.0:7409 run:app --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log --log-level debug
+exec gunicorn --workers 2 --bind 0.0.0.0:7409 run:app --access-logfile /var/log/gunicorn/access.log --error-logfile /var/log/gunicorn/error.log --log-level debug
