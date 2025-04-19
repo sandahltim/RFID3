53986d60b6418177ea22890cdd8f81ba52743c4a
sandahltim <tim@broadwaytent.com>
2025-04-18 20:16:52 -0500

    3609

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/tim/test_rfidpi/venv/bin/activate
-exec gunicorn --workers 1 --bind 0.0.0.0:8101 --chdir /home/tim/test_rfidpi run:app
+exec gunicorn --workers 1 --bind 0.0.0.0:3609 --chdir /home/tim/test_rfidpi run:app
