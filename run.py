bb1816a1c47a3cbaf6b873c356b4252312a68931
sandahltim <tim@broadwaytent.com>
2025-04-20 00:46:26 -0500

    grok is dumb

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from app import create_app
 
 app = create_app()
+@app.after_request
+def add_header(response):
+    if 'static' in response.location or 'static' in request.path:
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3609, debug=True)
