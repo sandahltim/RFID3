ab46bc37de1c0267caf9e128be2fd937bb1ae068
sandahltim <tim@broadwaytent.com>
2025-04-20 01:10:39 -0500

    1

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
+from flask import Flask, request
 from app import create_app
 
 app = create_app()
 
+# Prevent caching of static files by Cloudflare and browsers
+@app.after_request
+def add_header(response):
+    if 'static' in request.path:
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        # Prevent Cloudflare caching
+        response.headers['CF-Cache-Control'] = 'no-cache'
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3609, debug=True)
