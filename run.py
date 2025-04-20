5567d2223d4a1f282fa03544e7774ea40c6b73ae
sandahltim <tim@broadwaytent.com>
2025-04-20 00:55:20 -0500

    wtf

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from app import create_app
 
 app = create_app()
+
 @app.after_request
 def add_header(response):
-    if 'static' in response.location or 'static' in request.path:
+    # Check if response.location is None before using it
+    if ('static' in (response.location or '') or 'static' in request.path):
         response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
         response.headers['Pragma'] = 'no-cache'
         response.headers['Expires'] = '-1'
