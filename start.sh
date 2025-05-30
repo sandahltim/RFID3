c0e76a15ac21838c7093d4cb524cc5a7ee77c405
SMOK33Y3 <73911129+SMOK33Y3@users.noreply.github.com>
2025-05-30 13:32:07 -0500

    Reinstated Nginx on port 3607, fixed static file serving, updated base.html

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/tim/test_rfidpi/venv/bin/activate
-exec gunicorn --workers 3 --timeout 300 --bind 0.0.0.0:3607 --chdir /home/tim/test_rfidpi run:app
+exec gunicorn --workers 3 --timeout 300 --bind 127.0.0.1:3608 --chdir /home/tim/test_rfidpi run:app
