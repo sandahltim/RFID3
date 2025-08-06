1c2c35d85cceed2e1e96da6309564291b5bf1be5
sandahltim <tim@broadwaytent.com>
2025-08-06 16:27:44 -0500

    asd

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/tim/RFID3/venv/bin/activate
-exec gunicorn --workers 3 --timeout 300 --bind 127.0.0.1:8101 --chdir /home/tim/RFID3 run:app
+exec gunicorn --workers 3 --timeout 300 --bind 127.0.0.1:8102 --chdir /home/tim/RFID3 run:app
 
